285d86336df6ee0676fdcb58d720cd8851d987a3
Gabriel <g.cellardoor.delloiacovo@gmail.com>
2024-02-13 14:34:18 +0100

    Create setter method

--- Exercise.java
+++ Exercise.java
@@ -1,16 +1,9 @@
 public class Exercise {
     public static void main(String[] args) {
         Studente students1 = new Studente("Gabriel", "Dello Iacovo", 68954 );
-        //students1.name = "Gabriel";
-        //students1.surname = "Dello Iacovo";
-        //students1.id = 68954;
-
         Studente students2 = new Studente("Linda", "Trenta", 54687 );
-        //students2.name = "Linda";
-        //students2.surname = "Trenta";
-        //students2.id = 54687;
 
-        System.out.println("Student1 = " + students1);
+        System.out.println("Students details = " + students1);
         System.out.println("Student2 = " + students2);
     }
 
--- Studente.java
+++ Studente.java
@@ -1,13 +1,32 @@
 public class Studente {
-    private final String name;
+    private String name;
 
-    private final String surname;
+    private String surname;
 
-    private final int id;
+    private int id;
 
     public Studente (String name, String surname, int id) {
         this.name = name;
         this.surname = surname;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public String getSurname() {
+        return surname;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+    public void setId(int id) {
         this.id = id;
     }
 
