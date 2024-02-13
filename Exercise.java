7d0ee7c7990e9725de9d70a16507f0512884944b
Gabriel <g.cellardoor.delloiacovo@gmail.com>
2024-02-13 14:54:23 +0100

    Add comments and stamp

--- Exercise.java
+++ Exercise.java
@@ -3,8 +3,8 @@ public class Exercise {
         Studente students1 = new Studente("Gabriel", "Dello Iacovo", 68954 );
         Studente students2 = new Studente("Linda", "Trenta", 54687 );
 
-        System.out.println("Students details = " + students1);
-        System.out.println("Student2 = " + students2);
+        System.out.println("Students details = " + students1.getName() + " " + students1.getSurname() + " " + students1.getId());
+        System.out.println("Students details = " + students2.getName() + " " + students2.getSurname() + " " + students2.getId());
     }
 
 }
--- Studente.java
+++ Studente.java
@@ -1,16 +1,19 @@
 public class Studente {
+    //These variables are private so that no one from outside can modify them.
     private String name;
 
     private String surname;
 
     private int id;
 
+    // With this getter method I can be sure that when I create a student object it has the requested parameters.
     public Studente (String name, String surname, int id) {
         this.name = name;
         this.surname = surname;
         this.id = id;
     }
 
+    //With this method I can return the parameters I need from the object
     public String getName() {
         return name;
     }
@@ -20,6 +23,8 @@ public class Studente {
     public int getId() {
         return id;
     }
+
+    // With these methods setter I'm sure that the object gets the rights parameter
     public void setName(String name) {
         this.name = name;
     }
